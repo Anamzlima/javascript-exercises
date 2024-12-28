7d28f2b21d1c58b263c085c806f2c192c172e9e5
Anamzlima <anamzlima@gmail.com>
2024-12-28 12:53:12 -0300

    Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    
+    const validString = string
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphanumerical.includes(character))
+        .join('');
+    
+    const reversedString = validString.split('').reverse().join('');
+    
+    return validString === reversedString;
 };
 
 // Do not edit below this line

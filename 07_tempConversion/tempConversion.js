bb12d51387acd14be1e16d63b40d508d36c44cb3
Anamzlima <anamzlima@gmail.com>
2024-12-27 23:23:25 -0300

    Exercises 5 to 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  const celsiusTemperature = (temperature - 32) * 5/9;
+  
+  return parseFloat(celsiusTemperature.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  const fahrenheitTemp = temperature * 9/5 + 32;
+  return parseFloat(fahrenheitTemp.toFixed(1));
 };
 
 // Do not edit below this line

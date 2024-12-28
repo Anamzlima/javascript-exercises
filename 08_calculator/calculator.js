7eaeafe92f8083d95ec75afbdb6d216dbd5d4344
Anamzlima <anamzlima@gmail.com>
2024-12-28 12:53:12 -0300

    Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	return numArray.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  return numArray.reduce((product, current) => product * current);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let sum = 1;
+  for (let i = num; i > 0; i--) {
+    sum *= i;
+}
+  return sum;
 };
 
 // Do not edit below this line
